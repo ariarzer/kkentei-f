af2ed9c0f70f473a92a9a8e337aa48240bb31d0e
Sofi Valitova <ariarzer@gmail.com>
2025-04-30 21:30:31 +0300

    cors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from 'react';
+
+type Kanji = {
+  id: number;
+  kanji: string;
+};
+
 function App() {
-  return <h1>Kkentei</h1>;
+  const [kanji, setKanji] = useState<Kanji[]>([]);
+
+  useEffect(() => {
+    if (import.meta.env.MODE === 'development') {
+      fetch('http://localhost:3000', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+        },
+      }).then(res => {
+        res.json().then(data => setKanji(data));
+      });
+    } else {
+      fetch('https://kkentei-b.onrender.com', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+        },
+      }).then(res => {
+        res.json().then(data => setKanji(data));
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      {kanji.map(item => {
+        return <span key={item.id}>{item.kanji}</span>;
+      })}
+    </div>
+  );
 }
 
 export default App;
